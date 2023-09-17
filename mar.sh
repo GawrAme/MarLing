76993ba4442e8d9d0651d806c8e08900335e9c00
GawrAme <97426017+GawrAme@users.noreply.github.com>
2023-09-17 12:44:34 +0700

    Create mar.sh

---
+++ mar.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+sfile="https://github.com/GawrAme/MarLing"
+
+#domain
+read -rp "Masukkan Domain: " domain
+echo "$domain" > /root/domain
+domain=$(cat /root/domain)
+
+#Preparation
+clear
+cd;
+apt-get update;
+
+#Remove unused Module
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+#install bbr
+echo 'fs.file-max = 500000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.core.rmem_max=4000000
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.ip_forward=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
+sysctl -p;
+
+#install toolkit
+apt-get install libio-socket-inet6-perl libsocket6-perl libcrypt-ssleay-perl libnet-libidn-perl perl libio-socket-ssl-perl libwww-perl libpcre3 libpcre3-dev zlib1g-dev dbus iftop zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr dnsutils sudo at htop iptables bsdmainutils cron lsof lnav -y
+
+#Set Timezone GMT+7
+timedatectl set-timezone Asia/Jakarta;
+
+#Install Marzban
+sudo bash -c "$(curl -sL https://github.com/GawrAme/Marzban-scripts/blob/master/marzban.sh)" @ install
+
+#install env
+wget -O /opt/marzban/.env "$sfile/env"
+
+#install compose
+wget -O /opt/marzban/docker-compose.yml "$sfile/docker-compose.yml"
+
+#Install VNSTAT
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget $sfile/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install 
+cd
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz 
+rm -rf /root/vnstat-2.6
+
+#Install Speedtest
+apt-get install debian-archive-keyring curl gnupg apt-transport-https -y
+mkdir -p /etc/apt/keyrings/
+curl -fsSL https://packagecloud.io/ookla/speedtest-cli/gpgkey | gpg --dearmor > /etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg] https://packagecloud.io/ookla/speedtest-cli/debian/ `lsb_release -cs` main
+deb-src [signed-by=/etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg] https://packagecloud.io/ookla/speedtest-cli/debian/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ookla_speedtest-cli.list
+apt-get update
+apt install speedtest -y
+
+#install nginx
+apt install nginx -y
+rm /etc/nginx/conf.d/default.conf
+wget -O /etc/nginx/nginx.conf "$sfile/nginx.conf"
+wget -O /etc/nginx/conf.d/vps.conf "$sfile/vps.conf"
+wget -O /etc/nginx/conf.d/xray.conf "$sfile/xray.conf"
+systemctl enable nginx
+mkdir -p /var/www/html
+echo "<pre>Setup by LingVPN</pre>" > /var/www/html/index.html
+systemctl start nginx
+
+#install socat
+apt install iptables -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion -y
+
+#install cert
+systemctl stop nginx
+curl https://get.acme.sh | sh -s email=lingssh2@gmail.com
+/root/.acme.sh/acme.sh --server letsencrypt --register-account -m lingssh2@gmail.com --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
+systemctl start nginx
+wget -O /var/lib/marzban/xray_config.json "$sfile/xray_config.json"
+
+#install database
+wget -O /var/lib/marzban/db.sqlite3 "$sfile/db.sqlite3"
+
+#finishing
+apt autoremove -y
+apt clean
+marzban restart
