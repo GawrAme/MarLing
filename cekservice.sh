477bacbab5e6d7ea375b42ff1ba1bf38783957cc
GawrAme <97426017+GawrAme@users.noreply.github.com>
2023-09-21 08:49:33 +0700

    Create Check Service

---
+++ cekservice.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# // Code for service
+export RED='\033[0;31m';
+export GREEN='\033[0;32m';
+export YELLOW='\033[0;33m';
+export BLUE='\033[0;34m';
+export PURPLE='\033[0;35m';
+export CYAN='\033[0;36m';
+export LIGHT='\033[0;37m';
+export NC='\033[0m';
+
+# // Export Banner Status Information
+export ERROR="[${RED} ERROR ${NC}]";
+export INFO="[${YELLOW} INFO ${NC}]";
+export OKEY="[${GREEN} OKEY ${NC}]";
+export PENDING="[${YELLOW} PENDING ${NC}]";
+export SEND="[${YELLOW} SEND ${NC}]";
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]";
+
+# // VAR
+if [[ $(systemctl status nginx | grep -w running | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' | sed 's/ //g') == 'running' ]]; then
+    NGINX="${GREEN}Okay${NC}";
+else
+    NGINX="${RED}Not Okay${NC}";
+fi
+if [[ $(netstat -ntlp | grep -i python | grep -i 0.0.0.0:7879 | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g') == '7879' ]]; then
+    MARZ="${GREEN}Okay${NC}";
+else
+    MARZ="${RED}Not Okay${NC}";
+fi
+if [[ $(systemctl status ufw | grep -w Active | awk '{print $2}' | sed 's/(//g' | sed 's/)//g' | sed 's/ //g') == 'active' ]]; then
+    UFW="${GREEN}Okay${NC}";
+else
+    UFW="${RED}Not Okay${NC}";
+fi
+
+echo ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ Service Information ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ Nginx                :$NGINX"
+echo -e "❇️ Firewall             :$UFW"
+echo -e "❇️ Marzban Panel        :$MARZ"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "SHARING PORT 443 MARZBAN VERSION AMAN SEMUA BOSSKUH"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo ""
+
