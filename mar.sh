59c6973a503861f05187d38963b2571ce73fce41
GawrAme <97426017+GawrAme@users.noreply.github.com>
2024-05-11 11:26:15 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -134,14 +134,14 @@ net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_mem = 25600 51200 102400
 net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
-net.core.rmem_max=4000000
+net.core.rmem_max = 4000000
 net.ipv4.tcp_mtu_probing = 1
-net.ipv4.ip_forward=1
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-net.ipv6.conf.all.disable_ipv6=1
-net.ipv6.conf.default.disable_ipv6=1
-net.ipv6.conf.lo.disable_ipv6=1' >> /etc/sysctl.conf
+net.ipv4.ip_forward = 1
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
 sysctl -p;
 
 #install toolkit
