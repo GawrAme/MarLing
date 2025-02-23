b6db103efddcc10cc70fac21e8b112fca1d3f3a7
GawrAme <97426017+GawrAme@users.noreply.github.com>
2025-02-23 13:56:54 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -225,18 +225,18 @@ curl -X 'POST' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d "grant_type=password&username=${userpanel}&password=${passpanel}&scope=&client_id=string&client_secret=string" > /etc/data/token.json
 cd
-profile
 touch /root/log-install.txt
-echo "Untuk data login dashboard Marzban: " | tee -a /root/log-install.txt
-echo "-=================================-" | tee -a /root/log-install.txt
-echo "URL HTTPS : https://${domain}/dashboard" | tee -a /root/log-install.txt
-echo "username  : ${userpanel}" | tee -a /root/log-install.txt
-echo "password  : ${passpanel}" | tee -a /root/log-install.txt
-echo "-=================================-" | tee -a /root/log-install.txt
-echo "Jangan lupa join Channel & Grup Telegram saya juga di" | tee -a /root/log-install.txt
-echo "Telegram Channel: https://t.me/LingVPN" | tee -a /root/log-install.txt
-echo "Telegram Group: https://t.me/LingVPN_Group" | tee -a /root/log-install.txt
-echo "-=================================-" | tee -a /root/log-install.txt
+echo -e "Untuk data login dashboard Marzban: 
+-=================================-
+URL HTTPS : https://${domain}/dashboard 
+username  : ${userpanel}
+password  : ${passpanel}
+-=================================-
+Jangan lupa join Channel & Grup Telegram saya juga di
+Telegram Channel: https://t.me/LingVPN
+Telegram Group: https://t.me/LingVPN_Group
+-=================================-" > /root/log-install.txt
+profile
 colorized_echo green "Script telah berhasil di install"
 rm /root/mar.sh
 colorized_echo blue "Menghapus admin bawaan db.sqlite"
